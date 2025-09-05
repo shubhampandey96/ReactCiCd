d4d41b0b41b29dffd98ca01c42caa7422605dfe7
shubhampandey96 <shubhamofficial0763@gmail.com>
2025-09-05 19:08:00 +0530

    Added jenkinsfile

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'   // ✅ Import this
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    exclude: [...configDefaults.exclude, 'e2e/*'], // ✅ Now works
+  },
 })
